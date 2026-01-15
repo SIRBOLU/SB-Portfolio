f265baa44ae44fc92c06c882535272331bbb0a66
Sirbolu <peterbolu@yahoo.com>
2026-01-15 13:17:39 +0100

    continuation

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,15 +5,15 @@ import { faFile } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
   return (
     <div>
-      <header className="flex flex-wrap items-center justify-between pt-4 pb-4 pr-10 pl-10 bg-black text-white">
-        <div className="text-3xl font-extrabold">SB</div>
+      <header className="flex flex-wrap items-center justify-between pt-4 pb-4 pr-10 pl-10 bg-black text-white fixed left-0 right-0">
+        <div className="text-3xl font-extrabold">SB-techlyCoded</div>
         <div>
           <ul className="flex items-center flex-wrap gap-8">
             <li className="">Home</li>
             <li className="">Featured Projects</li>
             <li className="">About me</li>
             <li className="">Contact me </li>
-            <li className="bg-[#2dd4bf] text-black hover:bg-white p-2 pr-4 pl-4 rounded-md">
+            <li className="bg-[#2dd4bf] text-black hover:bg-white p-2 pr-4 pl-4 rounded-full">
               View Resume <FontAwesomeIcon icon={faFile} />
             </li>
           </ul>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faDisplay,
   faEnvelope,
+  faEnvelopeOpen,
+  faFile,
   faGlobe,
+  faLocationDot,
+  faPhone,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faGithub,
@@ -15,7 +19,7 @@ const Hero = () => {
   return (
     <div>
       <div>
-        <div className="bg-black h-[60vh] flex gap-4 flex-col justify-center">
+        <div className="bg-black h-[80vh] flex gap-4 flex-col justify-center">
           {/* <h1 className="text-4xl text-center text-[rgb(45,212,191)]">
             {`<Hello/>`} 👋 {"<Hello/>"}
           </h1> */}
@@ -45,7 +49,7 @@ const Hero = () => {
         <div className="bg-[#27272a] p-10 ">
           <div className="text-center [&>*]:text-white">
             <p>SERVICES</p>
-            <p className="text-white pt-2 pb-8">
+            <p className="text-white pt-2 pb-8 text-3xl">
               What <span className="text-[rgb(45,212,191)]">I do.</span>
             </p>
           </div>
@@ -72,6 +76,102 @@ const Hero = () => {
                 appeal with seamless functionality, grounded in user-centered
                 and responsive design principles.
               </p>
+            </div>
+          </div>
+          <div>
+            <p>PORTFOLIO</p>
+            <p>
+              Featured <span>Projects</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-[800px] m-auto pt-10 pb-10">
+        <p className="text-white text-center">MY BIO</p>
+        <p className="text-center text-white text-3xl pb-6">
+          About <span className="text-[rgb(45,212,191)]">me</span>.
+        </p>
+        <div className="flex gap-[3rem]">
+          <img
+            src="./src/assets\profile-pic.png"
+            alt="my-pic"
+            className="w-[200px] h-[200px]"
+          />
+          <p className="text-white text-sm">
+            I am a motivated and results-driven web developer, with a good
+            knowledge of HTML, CSS, Tailwind CSS, JavaScript, and ReactJS,
+            seeking an opportunity to add value by building responsive and
+            scalable user interfaces. I am detail-oriented, a fast learner, and
+            a collaborative team player, eager to grow under experienced
+            professionals. I am also a team player, committed to building
+            engaging digital experiences that enhance user satisfaction, while
+            ultimately supporting organization’s mission.
+            <div>
+              <button className="bg-[#2dd4bf] text-black hover:bg-white p-2 pr-4 pl-4 rounded-full mt-4">
+                View Resume <FontAwesomeIcon icon={faFile} />
+              </button>
+            </div>
+          </p>
+        </div>
+        <div className="text-white flex justify-between gap-12 mt-14 mb-14 max-w-[800px] ">
+          <div className="w-[350px]">
+            <p>SKILLS</p>
+            <p className="pb-4 text-3xl">
+              Technical <span className="text-[rgb(45,212,191)]">Skills</span>
+            </p>
+            <p className="text-sm leading-6">
+              Frontend Developer skilled in building responsive, accessible, and
+              user-centric web interfaces using HTML, CSS, JavaScript, React,
+              and Tailwind CSS. Experienced in translating designs into
+              performant, scalable, and maintainable code.
+            </p>
+          </div>
+          <div className="w-[350px] flex flex-col gap-4 justify-center text-xs">
+            <div className="flex justify-evenly">
+              <div className="w-[100px] text-center bg-[#27272a] p-4 border-2 border-[rgb(45,212,191)] rounded-lg ">
+                HTML5
+              </div>
+              <div className="w-[100px] text-center bg-[#27272a] p-4 border-2 border-[rgb(45,212,191)] rounded-lg ">
+                CSS3
+              </div>
+            </div>
+            <div className="flex justify-evenly">
+              <div className="w-[100px] text-center bg-[#27272a] p-4 border-2 border-[rgb(45,212,191)] rounded-lg ">
+                JAVASCRIPT
+              </div>
+              <div className="w-[100px] text-center bg-[#27272a] p-4 border-2 border-[rgb(45,212,191)] rounded-lg ">
+                REACT
+              </div>
+            </div>
+            <div className="flex justify-evenly">
+              <div className="w-[100px] text-center bg-[#27272a] p-4 border-2 border-[rgb(45,212,191)] rounded-lg ">
+                TAILWIND
+              </div>
+              <div className="w-[100px] text-center bg-[#27272a] p-4 border-2 border-[rgb(45,212,191)] rounded-lg ">
+                UI DESIGN
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-white bg-[#27272a] p-8">
+          <div className="text-center">
+            <p>GET IN TOUCH</p>
+            <p className="text-3xl pb-4">
+              Contact <span className="text-[rgb(45,212,191)]">me</span>.
+            </p>
+            <p>
+              I am currently open to full-time, part-time, contract or remote
+              opportunities in Frontend Development.
+            </p>
+          </div>
+          <div>
+            <div>
+              <p>Have an awesome project idea?</p>
+              <p>Let's Discuss</p>
+              <FontAwesomeIcon icon={faPhone} /> +234 813 030 0514 <br />
+              <FontAwesomeIcon icon={faEnvelopeOpen} />{" "}
+              peterbolu.akintola@gmail.com <br />
+              <FontAwesomeIcon icon={faLocationDot} /> Ibadan, Oyo State.
             </div>
           </div>
         </div>
