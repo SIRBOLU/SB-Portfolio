c0856e65a47e6ef95c093c0c6a12fa6d52170ad8
Sirbolu <peterbolu@yahoo.com>
2026-01-18 03:28:37 +0100

    write-up complete

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,6 +6,7 @@ import {
   faEnvelopeOpen,
   faFile,
   faGlobe,
+  faInbox,
   faLocationDot,
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
@@ -19,6 +20,7 @@ const Hero = () => {
   return (
     <div>
       <div>
+        {/* INTRODUCTION */}
         <div className="bg-black h-[80vh] flex gap-4 flex-col justify-center">
           {/* <h1 className="text-4xl text-center text-[rgb(45,212,191)]">
             {`<Hello/>`} 👋 {"<Hello/>"}
@@ -46,6 +48,8 @@ const Hero = () => {
             <FontAwesomeIcon icon={faTwitter} />
           </div>
         </div>
+
+        {/* SERVICES */}
         <div className="bg-[#27272a] p-10 ">
           <div className="text-center [&>*]:text-white">
             <p>SERVICES</p>
@@ -78,14 +82,18 @@ const Hero = () => {
               </p>
             </div>
           </div>
-          <div>
+
+          {/* PORTFOLIO */}
+          <div className="text-white text-center my-12">
             <p>PORTFOLIO</p>
             <p>
-              Featured <span>Projects</span>
+              Featured <span className="text-[rgb(45,212,191)]">Projects</span>
             </p>
           </div>
         </div>
       </div>
+
+      {/* MY BIO */}
       <div className="max-w-[800px] m-auto pt-10 pb-10">
         <p className="text-white text-center">MY BIO</p>
         <p className="text-center text-white text-3xl pb-6">
@@ -97,7 +105,7 @@ const Hero = () => {
             alt="my-pic"
             className="w-[200px] h-[200px]"
           />
-          <p className="text-white text-sm">
+          <p className="text-white text-sm leading-7">
             I am a motivated and results-driven web developer, with a good
             knowledge of HTML, CSS, Tailwind CSS, JavaScript, and ReactJS,
             seeking an opportunity to add value by building responsive and
@@ -116,10 +124,10 @@ const Hero = () => {
         <div className="text-white flex justify-between gap-12 mt-14 mb-14 max-w-[800px] ">
           <div className="w-[350px]">
             <p>SKILLS</p>
-            <p className="pb-4 text-3xl">
+            <p className="pb-4 pt-2 text-3xl">
               Technical <span className="text-[rgb(45,212,191)]">Skills</span>
             </p>
-            <p className="text-sm leading-6">
+            <p className="text-sm leading-7">
               Frontend Developer skilled in building responsive, accessible, and
               user-centric web interfaces using HTML, CSS, JavaScript, React,
               and Tailwind CSS. Experienced in translating designs into
@@ -153,6 +161,8 @@ const Hero = () => {
             </div>
           </div>
         </div>
+
+        {/* CONTACT ME */}
         <div className="text-white bg-[#27272a] p-8">
           <div className="text-center">
             <p>GET IN TOUCH</p>
@@ -164,15 +174,106 @@ const Hero = () => {
               opportunities in Frontend Development.
             </p>
           </div>
-          <div>
+
+          {/* CONTACT ME DETAILS */}
+          <div className="flex justify-between bg-black p-10 mt-6 mb-6 text-sm rounded-lg">
             <div>
               <p>Have an awesome project idea?</p>
-              <p>Let's Discuss</p>
-              <FontAwesomeIcon icon={faPhone} /> +234 813 030 0514 <br />
-              <FontAwesomeIcon icon={faEnvelopeOpen} />{" "}
-              peterbolu.akintola@gmail.com <br />
-              <FontAwesomeIcon icon={faLocationDot} /> Ibadan, Oyo State.
+              <p className="text-[rgb(45,212,191)] pb-6">Let's Discuss</p>
+              <div className="flex gap-2 items-center pb-6 ">
+                <FontAwesomeIcon icon={faPhone} />
+                <p className="hover:text-[rgb(45,212,191)]">
+                  +234 813 030 0514
+                </p>
+              </div>
+              <div className="flex gap-2 items-center pb-6">
+                <FontAwesomeIcon icon={faEnvelopeOpen} />
+                <p className="hover:text-[rgb(45,212,191)]">
+                  peterbolu.akintola@gmail.com
+                </p>
+              </div>
+              <div className="flex gap-2 items-center">
+                <FontAwesomeIcon icon={faLocationDot} />
+                <p className="hover:text-[rgb(45,212,191)]">
+                  Ibadan, Oyo State.
+                </p>
+              </div>
             </div>
+
+            <form action="" className="text-left">
+              <div className="flex flex-col">
+                <label htmlFor="" className="pb-2">
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter name"
+                  className="w-[300px] rounded-md p-2 bg-[#27272a] border-2 border-white"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="" className="pb-2 pt-4">
+                  Email Address
+                </label>
+                <input
+                  type="emaii"
+                  placeholder="Enter email"
+                  className="rounded-md p-2 bg-[#27272a] border-2 border-white"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="" className="pb-2 pt-4">
+                  Message
+                </label>
+                <textarea
+                  name=""
+                  id=""
+                  placeholder="Enter message"
+                  className="rounded-md p-2 bg-[#27272a] border-2 border-white h-28"
+                ></textarea>
+              </div>
+
+              <button className="mt-6 bg-[rgb(45,212,191)] px-3 py-2 rounded-lg text-black">
+                Send Message <FontAwesomeIcon icon={faInbox} />
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-white text-xs flex justify-around my-8">
+        <div className="text-3xl font-extrabold text-white">SB-techlyCoded</div>
+        <div className="flex flex-col gap-3">
+          <p>Web Developer</p>
+          <p>Frontend Developer</p>
+        </div>
+        <div>
+          <ul className="flex flex-col gap-3">
+            <li className="hover:text-[rgb(45,212,191)]">Home</li>
+            <li className="hover:text-[rgb(45,212,191)]">My projects</li>
+            <li className="hover:text-[rgb(45,212,191)]">About me</li>
+            <li className="hover:text-[rgb(45,212,191)]">Contact me</li>
+          </ul>
+        </div>
+        <div>
+          <p>Social Media Handles</p>
+          <div className=" text-center flex justify-center gap-4 mt-4 [&>*]:bg-gray-200 [&>*]:rounded-full [&>*]:p-2 text-black">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="hover:bg-[rgb(45,212,191)]"
+            />
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className="hover:bg-[rgb(45,212,191)]"
+            />
+            <FontAwesomeIcon
+              icon={faLinkedinIn}
+              className="hover:bg-[rgb(45,212,191)]"
+            />
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className="hover:bg-[rgb(45,212,191)]"
+            />
           </div>
         </div>
       </div>
